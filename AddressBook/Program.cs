53c07865290f535321bf3175d21e6050a3da303e
rutuja patil <rutujapatil991@gmail.com>
2022-03-17 15:46:33 +0530

    Adding Data

---
+++ AddressBook/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                //Showing Welcome Message On console.
+                Console.WriteLine("Welcome to Address Book Progam \n");
+
+                Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+                CreateAddressBook addressBook = new CreateAddressBook()  //Initializing elements using collection-initializer syntax
+                {
+                    FirstName = Console.ReadLine(),
+                    LastName = Console.ReadLine(),
+                    Address = Console.ReadLine(),
+                    City = Console.ReadLine(),
+                    State = Console.ReadLine(),
+                    Zip = Convert.ToDouble(Console.ReadLine()),
+                    PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                    Email = Console.ReadLine(),
+                };
+                addressBook.displayContact();
+                Console.ReadLine();
+            }
+        }
+    }
+}
